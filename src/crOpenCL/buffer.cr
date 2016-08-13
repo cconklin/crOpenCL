fcae8ef608f0da0432142c5c02182d280f631db6
Chase Conklin <chase@conklins.net>
2016-08-13 14:09:20 -0400

    redo buffer interface for more convience

--- src/crOpenCL/buffer.cr
+++ src/crOpenCL/buffer.cr
@@ -23,10 +23,11 @@ module CrOpenCL
 
     # If access allows reading, and the buffer has not been copied,
     # it will be copied directly prior to kernel launch on the kernel's command queue
-    def initialize(@context : Context, @access : Memory, *, hostbuf : Array(T), length : UInt64 = 0)
+    def initialize(@context : Context, @access : Memory, *, hostbuf : (Array(T) | Nil) = nil, length = 0)
       # Tested this in the playground: even though kind holds the type of the array element
       # typeof is needed for this to work. It correctly gets the size of the type
       @length = (hostbuf.nil? ? length : hostbuf.size).to_u64
+      raise CLError.new("Buffer cannot be zero size.") if @length == 0
       @hostbuf = hostbuf
       @size = @length * sizeof(T)
       # Create the buffer
@@ -34,20 +35,47 @@ module CrOpenCL
       raise CLError.new("clCreateBuffer failed.") unless alloc_err == CL_SUCCESS
     end
 
-    def enqueue_copy(queue : CommandQueue, direction : Transfer, *, blocking : (Bool | Nil) = nil)
+    def self.enqueue_copy(queue : CommandQueue, hostbuf : Slice(T), devbuf : Buffer, length : UInt64, direction : Transfer, *, blocking = false)
+      blocking = blocking ? 1 : 0
       if direction == Transfer::ToHost
-        blocking ||= true
-        blocking = blocking ? 1 : 0
-        # If there is a host_buffer, use it instead
-        dest_ary = @hostbuf.nil? ? Array(T).new(@length) : @hostbuf
-        err = LibOpenCL.clEnqueueReadBuffer(queue, @buffer, blocking.to_i32, 0, @size, dest_ary, 0 , nil, nil)
+        err = LibOpenCL.clEnqueueReadBuffer(queue, devbuf, blocking.to_i32, 0, length * sizeof(T),  hostbuf , 0 , nil, nil)
         raise CLError.new("clEnqueueWriteBuffer failed.") unless err == CL_SUCCESS
-        return dest_ary
       else
-        blocking = blocking ? 1 : 0
-        raise CLError.new("No host buffer to copy.") if @hostbuf.nil?
-        err = LibOpenCL.clEnqueueWriteBuffer(queue, @buffer, blocking.to_i32, 0, @size, @hostbuf, 0 , nil, nil)
+        err = LibOpenCL.clEnqueueWriteBuffer(queue, devbuf, blocking.to_i32, 0, length * sizeof(T), hostbuf, 0 , nil, nil)
         raise CLError.new("clEnqueueWriteBuffer failed.") unless err == CL_SUCCESS
+      end
+    end
+
+    def set(queue : CommandQueue, hostbuf : Array(T), blocking = false)
+      slice = Slice(T).new(hostbuf.to_unsafe, hostbuf.size)
+      Buffer(T).enqueue_copy(queue, slice, self, hostbuf.size.to_u64, Transfer::ToDevice, blocking: blocking)
+    end
+
+    def set(queue : CommandQueue, blocking = false)
+      raise CLError.new("No host buffer to copy.") if @hostbuf.nil?
+      slice = Slice(T).new(@hostbuf.as(Array(T)).to_unsafe, @hostbuf.as(Array(T)).size)
+      Buffer(T).enqueue_copy(queue, slice, self, slice.size.to_u64, Transfer::ToDevice, blocking: blocking)
+    end
+
+    def get(queue : CommandQueue, hostbuf : Array(T), *, blocking : Bool)
+      slice = Slice(T).new(hostbuf.to_unsafe, hostbuf.size)
+      Buffer(T).enqueue_copy(queue, slice, self, hostbuf.size.to_u64, Transfer::ToHost, blocking: blocking)
+      nil
+    end
+
+    def get(queue : CommandQueue, hostbuf : Array(T))
+      get(queue, hostbuf, blocking: true)
+      return hostbuf
+    end
+
+    def get(queue : CommandQueue)
+      if @hostbuf.nil?
+        # Doing this instead of creating the array directly gives the array the correct size
+        # i.e. the array knows it has @length elements
+        buf = Slice(T).new(@length).to_a
+        get(queue, buf)
+      else
+        get(queue, @hostbuf.as(Array(T)))
       end
     end
 
